6123e482f7dbf8575e93a4c637daa68f5d8f8fc6
Daniel Bustillos <bustillosdaniel4@gmail.com>
2022-12-04 14:42:10 -0300

    test

--- app.js
+++ app.js
@@ -4,18 +4,18 @@ import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 const app = express();
 const port = process.env.PORT | 3000;
 
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(bodyParser.json());
+// app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.use("/gifts", giftRouter);
+// app.use("/gifts", giftRouter);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
