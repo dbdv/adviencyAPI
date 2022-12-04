9d6e4c158a1e986fa969cae46fb9014603e994be
Daniel Bustillos <bustillosdaniel4@gmail.com>
2022-12-04 15:18:33 -0300

    morgan

--- app.js
+++ app.js
@@ -3,13 +3,15 @@ import giftRouter from "./routes/gifts.route.js";
 import bodyParser from "body-parser";
 import cors from "cors";
 import dotenv from "dotenv";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT | 3000;
+const port = process.env.PORT | 4000;
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(morgan("tiny"));
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
